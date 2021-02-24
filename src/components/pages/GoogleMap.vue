7e7f868e569dd1977a09ceb54a9faeae29e69aac
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-02-24 15:16:51 +0000

    fixed google maps for production

--- src/components/pages/GoogleMap.vue
+++ src/components/pages/GoogleMap.vue
@@ -2,10 +2,11 @@
   <div id="map" ref="mapDiv">
     <LoadingSpinner v-if="isLoading" />
   </div>
+  <a href=""></a>
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 import LoadingSpinner from "../UI/LoadingSpinner.vue";
 export default {
   components: { LoadingSpinner },
@@ -16,83 +17,84 @@ export default {
       Google_api_key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY,
       isLoading: false,
       Data: null,
+      Marker: null,
     };
   },
   mounted() {
-    //* Calling the Map-details with GoogleId
-    this.isLoading = true;
-    // const URL = `http://localhost:8080/maps/api/place/details/json?key=${this.Google_api_key}&place_id=${this.googleId}`;
-    const URL = `https://lucid-villani-555a83.netlify.app/maps/api/place/details/json?key=${this.Google_api_key}&place_id=${this.googleId}`;
-    // const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?key=${this.Google_api_key}&place_id=${this.googleId}`;
-    axios
-      .get(URL)
-      .then((response) => {
-        if (response.data.error_message) {
-          this.error = response.data.error_message;
-        } else {
-          const place = response.data.result;
-          return place;
-        }
-      })
-      //* Getting the Data from responce
-      .then((place) => {
+    const map2 = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: -33.9142686, lng: 18.0955572 },
+      zoom: 14,
+      mapTypeId: "hybrid",
+      mapTypeControl: false,
+    });
+    var request = {
+      placeId: this.googleId,
+      fields: [
+        "name",
+        "rating",
+        "formatted_phone_number",
+        "geometry",
+        "website",
+      ],
+    };
+    const service = new window.google.maps.places.PlacesService(map2);
+    service.getDetails(request, callback);
+    function callback(place, status) {
+      console.log(place);
+      if (status == window.google.maps.places.PlacesServiceStatus.OK) {
+        const map = new window.google.maps.Map(document.getElementById("map"), {
+          center: place.geometry.location,
+          zoom: 14,
+          mapTypeId: "hybrid",
+          mapTypeControl: false,
+        });
+        const marker = new window.google.maps.Marker({
+          map: map,
+          position: place.geometry.location,
+          title: place.name,
+        });
+
         const data = {
-          lat: place.geometry.location.lat,
-          lng: place.geometry.location.lng,
+          position: place.geometry.location,
           Address: place.formatted_address,
-          Phone: place.international_phone_number,
+          Phone: place.formatted_phone_number,
           name: place.name,
           rating: place.rating,
           website: place.website,
           total_rating: place.user_ratings_total,
         };
-        this.Data = data;
-
-        //**Initialise Map*/
-        let map = new window.google.maps.Map(this.$refs["mapDiv"], {
-          center: new window.google.maps.LatLng(data.lat, data.lng),
-          zoom: 15,
-          mapTypeId: "hybrid",
-          mapTypeControl: false,
-        });
+        // this.Data = data;
         //**Initialise InfoWindow*/
         const infoWindow = new window.google.maps.InfoWindow();
-        let marker = new window.google.maps.Marker({
-          position: new window.google.maps.LatLng(data.lat, data.lng),
-          map: map,
-        });
-        this.Marker = marker;
-        //**Populate InfoWindow */
         if (
-          this.Data.name === "Kalk Bay" ||
-          this.Data.name === "Constantia" ||
-          this.Data.name === "Bo-Kaap"
+          data.name === "Kalk Bay" ||
+          data.name === "Constantia" ||
+          data.name === "Bo-Kaap"
         ) {
           infoWindow.setContent(
-            `<div class="info_Header"><h2>${this.Data.name}</2h></div>
-            <div class="info_Data"><h5>Address: ${this.Data.Address}</h5><div>
+            `<div class="info_Header"><h2>${data.name}</2h></div>
+            
             `
           );
           infoWindow.open(map, marker);
           this.isLoading = false;
         } else {
           infoWindow.setContent(
-            `<div class="info_Header"><h2>${this.Data.name}</2h></div>
-            <div class="info_Data"><h5>Address: ${this.Data.Address}</h5><div>
-            <div v-if="noPhone" class="info_Data"><h5>Phone: ${this.Data.Phone}</h5><div>
-            <div class="info_Data"><h5>Ratings: ${this.Data.rating} /5 <small>Total Ratings ${this.Data.total_rating}</small></h5><div>
-            <a class="info_Data"> <h5>WebSite: ${this.Data.website}</a>`
+            `<div class="info_Header"><h2>${data.name}</2h></div>
+            <div v-if="noPhone" class="info_Data"><h5>Phone: ${data.Phone}</h5><div>
+            <div class="info_Data"><h5>Ratings: ${data.rating} /5 <small>Total Ratings ${data.total_rating}</small></h5><div>
+            <h5>WebSite: <a href="${data.website}" target="_blank" rel="noopener noreferrer">${data.website}</a></h5`
           );
           infoWindow.open(map, marker);
           this.isLoading = false;
         }
-      })
-      .catch((error) => {
-        this.error = error.message;
-      });
+      } else {
+        console.log("ERROR");
+      }
+    }
   },
   methods: {
-    //** Getting User Loaction */
+    //** Getting User Loaction and show directions*/
     getRoute() {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
